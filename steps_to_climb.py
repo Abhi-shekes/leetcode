0a4645f858dddf2260c071ae31ab617bab25954c
Filament-AI <filamentaiservices@gmail.com>
2024-07-14 14:46:00 +0530

    day13

---
+++ steps_to_climb.py
@@ -0,0 +1,8 @@
+class Solution:
+    def stepclimb(n):
+        one, two =1,1
+        for i in range(n-1):
+            temp = one 
+            one = one + two
+            two = temp 
+        return one
